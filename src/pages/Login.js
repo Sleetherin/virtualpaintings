0de8ab20da096b44e61edc6736e283a614cd377e
PartaCana <kalthidra@gmail.com>
2024-12-03 15:43:48 +0100

    started working on the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styledepartment/Login.css'; 
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username, 'Password:', password);
+  };
+
   return (
-    <div>
-      
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="input-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit" className="login-button">Log In</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
+
